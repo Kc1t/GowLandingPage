70d5e149d5df9bb1528b29deaeb2d68a0ec18769
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-02-02 09:34:43 -0300

    SpotifyEmbed

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,35 @@ import Nav from "./components/Nav"
 import Ini from "./components/Ini"
 import Sobre from "./components/Sobre"
 import Back from './img/Fundo_topo.png'
+import SpotifyPlayer from 'react-spotify-player';
 
 function App() {
 
+  const size = {
+    width: '100%',
+    height: 300,
+  };
+  const view = 'list';
+  const theme = 'black'; 
+  
+
   return (
+
     <section>
     <div className="Home" style={{ backgroundImage:`url(${Back})`, backgroundPosition: 'center center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
       <Nav/>
       <Ini/>
     </div>
-
     <div>
-    <Sobre/>
+      <Sobre/>
+      <SpotifyPlayer
+        uri="spotify:album:1TIUsv8qmYLpBEhvmBmyBk"
+        size={size}
+        view={view}
+        theme={theme}
+      />
     </div>
-    
+
     </section>
   )
 }
--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -18,7 +18,12 @@ const Sobre = () => {
       <div className="Text">
         <h2>Sobre a Trilha</h2>
         <h4>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione impedit aliquam tempore modi veniam! Harum, sequi dicta? Mollitia earum voluptatum, magni eum molestiae ex! Nobis, officia est ipsa molestiae expedita, harum voluptas optio alias corporis maxime voluptatibus obcaecati voluptate vero!
+        "    Quando o Santa Monica Studio me pediu para compor a trilha do novo God of War, fiquei emocionado, já que sou um grande fã dos jogos originais da franquia.<br></br>
+        <br></br> Conforme colaborava com o diretor do jogo Cory Barlog, ficou claro que o jogo novo precisaria de música nova, sem revisitar a trilha memorável dos games originais, em vez disso, teríamos que trilhar um novo terreno. <br></br>
+        <br></br>
+         Usei as memórias que tinha da trilha clássica do antigo God of War – o coro profundo, percussão forte, e o metal gritante – e os reinventei para uma era Nórdica. Escrevi novos temas, e introduzi instrumentação exótica e línguas de várias tradições do norte da Europa à música. "<br></br>
+        <br></br>
+        -Bear Mccreary
         </h4>
       </div>
     </div>
