d20007f9659e9cdce2490619b188d65a60c730b0
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-02-03 17:17:19 -0300

    Topic

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -5,7 +5,9 @@ const Topic = () => {
   return (
     <div className='Topic'>
 
+    <div className="Freys">
         <img src={Frey} alt="" />
+    </div>
 
     <div className="TextArea">
         
@@ -13,6 +15,9 @@ const Topic = () => {
 
       <h5>
       Estúdio é composto por um grupo de criativos diversificados, ambiciosos e altamente colaborativos que se capacitam mutuamente, enquanto trabalham em equipe para oferecer experiências PlayStation de alta qualidade baseadas em narrativas. Com origens em todo o mundo - do Brasil à Polônia e à pequena cidade dos EUA, somos o lar de mais de 250 desenvolvedores de classe mundial que se esforçam para impulsionar o meio a cada decisão criativa.
+
+    <br></br>
+    <br></br>
 
     Centro de nossa história, estamos comprometidos com a grandeza e firmes em nosso ofício de desenvolver experiências inesquecíveis no jogo que inspiram nossos jogadores. Além disso, porém, somos movidos por algo muito maior... pelo desejo de desenvolver “histórias” de grandeza para cada membro da equipe. Fazer um jogo é uma tarefa enorme. Entre QA, Arte, Design, Programação, Produção, Som e todos os departamentos intermediários, todos somos responsáveis por conectar as maiores habilidades uns dos outros, enquanto trabalhamos em direção a uma visão comum.
 
